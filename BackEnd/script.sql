69f682c744c3174db00e4e5dd33327ccfc7ddab6
sarah! <sarah.c.nascimento10@gmail.com>
2026-06-01 17:15:56 -0300

    01-06
    conseguimos rodar bem, mas as tabelas precisam ser alteradas e feitas de forma
    correta

--- BackEnd/script.sql
+++ BackEnd/script.sql
@@ -1,30 +1,66 @@
-//---------- tabela da lista dos cursos -----------
-
-CREATE TABLE IF NOT EXISTS public.lista_curso
+//----- tab de usuario
+CREATE TABLE IF NOT EXISTS public.tab_cursos
 (
     id_curso uuid NOT NULL DEFAULT uuid_generate_v4(),
-    nome_curso text COLLATE pg_catalog."default" NOT NULL,
-    categoria_curso text COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT lista_curso_pkey PRIMARY KEY (id_curso)
-)
+    nome_curso text NOT NULL,
+    tempo_curso character varying NOT NULL,
+    profe_curso text NOT NULL,
+    materia_curso text NOT NULL,
+    PRIMARY KEY (id_curso)
+);
 
-//-------------- tabela do cadastro ------------
-
-CREATE TABLE IF NOT EXISTS public.usuarios_cadastro
+//------ tab  de cursos
+CREATE TABLE IF NOT EXISTS public.tab_usuario
 (
     id_usuario uuid NOT NULL DEFAULT uuid_generate_v4(),
     nome_usuario text COLLATE pg_catalog."default" NOT NULL,
     email_usuario character varying COLLATE pg_catalog."default" NOT NULL,
+    senha_usuario character varying COLLATE pg_catalog."default" NOT NULL,
     telefone_usuario character varying COLLATE pg_catalog."default" NOT NULL,
-    senha_usuario numeric NOT NULL,
-    CONSTRAINT usuarios_cadastro_pkey PRIMARY KEY (id_usuario)
-)
+    CONSTRAINT tab_cadastro_pkey PRIMARY KEY (id_usuario)
+);
 
-//----------------- tabela do login ---------------
 
-CREATE TABLE IF NOT EXISTS public.usuarios_login
-(
-    email_usuario character varying COLLATE pg_catalog."default" NOT NULL,
-    senha_usuario numeric NOT NULL,
-    CONSTRAINT usuarios_login_pkey PRIMARY KEY (email_usuario)
-)
+//--- tabela de cadastro
+-- id - PK
+-- email 
+-- senha
+-- confirmar senha
+-- nome 
+-- telefone
+
+-- //---------tabela de login 
+
+-- id - PK
+-- nome
+-- email
+-- recuperar senha (por id)
+-- localização
+-- descrição bio
+
+-- FK -  tabela de cadastro: conectar senha para recuperação
+
+-- //---------tabela de usuario (perfil)
+-- id - PK id usuario
+-- email
+-- nome - tab de usuario
+-- atualizar senha 
+-- localização
+-- descrição bio
+
+-- FK - tabela de cursos
+
+-- //------tabela de curso
+-- id - PK
+-- nome do curso
+-- tempo do curso 
+-- profe do curso 
+-- materia 
+
+-- FK - tabela de usuario: para sabermos qual curso o usuario acessa
+
+
+-- // p apagar tab
+
+-- DELETE FROM public.tab_cadastro
+-- 	WHERE id_cadastro = 'e96dd98e-b266-4f5b-b372-4d472275dd41'
